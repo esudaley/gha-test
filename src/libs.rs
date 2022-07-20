fb69ee38cdb42227dcd461c8007f23c15178d1ab
esudaley <eli.sudaley@gmail.com>
2022-07-20 13:49:46 -0400

    try again

--- src/libs.rs
+++ src/libs.rs
@@ -1,12 +1,12 @@
-#[pyfunction]
-fn abc() {
-    println!("Hello, world!");
-}
+// #[pyfunction]
+// fn abc() {
+//     println!("Hello, world!");
+// }
 
-#[pymodule]
-#[pyo3(name = "allen")]
-fn allen(_py: Python, m: &PyModule) -> PyResult<()> {
-    pyo3_log::init();
-    m.add_wrapped(wrap_pyfunction!(abc)).unwrap();
-    Ok(())
-}
+// #[pymodule]
+// #[pyo3(name = "allen")]
+// fn allen(_py: Python, m: &PyModule) -> PyResult<()> {
+//     pyo3_log::init();
+//     m.add_wrapped(wrap_pyfunction!(abc)).unwrap();
+//     Ok(())
+// }
