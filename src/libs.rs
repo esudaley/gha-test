6cc18aff1ed1be6d484f4a44702339d3af258c1d
esudaley <eli.sudaley@gmail.com>
2022-07-20 13:47:49 -0400

    mock lib

--- src/libs.rs
+++ src/libs.rs
@@ -1,5 +1,12 @@
+#[pyfunction]
+fn abc() {
+    println!("Hello, world!");
+}
+
 #[pymodule]
 #[pyo3(name = "allen")]
-fn main() {
-    println!("Hello, world!");
+fn allen(_py: Python, m: &PyModule) -> PyResult<()> {
+    pyo3_log::init();
+    m.add_wrapped(wrap_pyfunction!(abc)).unwrap();
+    Ok(())
 }
