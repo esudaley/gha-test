b73515d39a9da50db23903b4018243376751cee1
esudaley <eli.sudaley@gmail.com>
2022-08-12 11:31:54 -0400

    ta

--- api/test-api.py
+++ api/test-api.py
@@ -11,6 +11,5 @@ r.set("test", "Hello World!")
 @app.get("/")
 def root ():
 # create connection to the redis cluster
-  test = r.get(test)
-  r.ping()
+  test = r.get("test")
   return {"message": test}
