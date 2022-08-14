613cbf3b435d939b5655e08c81f9299456e32a32
esudaley <eli.sudaley@gmail.com>
2022-08-14 12:09:24 -0400

    Mock app

---
+++ api/api.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import redis 
+import os
+
+app = FastAPI()
+
+# h = "redis" if "INSIDEDOCKER" in os.environ else "localhost"
+# print(h)
+# r = redis.Redis(host=h, port=6379)
+# r.set("test", "Hello World!")
+
+@app.get("/")
+def root ():
+# create connection to the redis cluster
+  return {"message": "Hello World!"}
+# def root ():
+# # create connection to the redis cluster
+#   test = r.get("test")
+#   return {"message": test}
